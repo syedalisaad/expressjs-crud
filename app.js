0bb6a0f554d6aadcf6b946cc3562faeee62e0080
syedalisaad <60425859+syedalisaad@users.noreply.github.com>
2023-06-07 16:28:22 +0500

    swagger
    git

--- app.js
+++ app.js
@@ -1,64 +1,71 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
 var path = require('path');
 var flash = require('express-flash');
 var session = require('express-session');
 var mysql = require('mysql');
-var connection  = require('./lib/db');
-var usersRouter = require('./routes/users');
-var app = express();
-const express = require("express"),
-  bodyParser = require("body-parser"),
+var connection = require('./config/db.config');
+var usersRouter = require('./routes/user.routes');
+var authRouter = require('./routes/auth.routes');
+const swaggerFile = require('./swagger_output.json')
+const bodyParser = require("body-parser"),
   swaggerJsdoc = require("swagger-jsdoc"),
   swaggerUi = require("swagger-ui-express");
 const options = {
-    definition: {
-      openapi: "3.1.0",
-      info: {
-        title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
-        description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        },
-     
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
       },
-      servers: [
-        {
-          url: "http://localhost:3000",
-        },
-      ],
-    },
-    apis: ["./routes/*.js"],
-  };
-  
-  const specs = swaggerJsdoc(options);
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ 
-    cookie: { maxAge: 60000 },
-    store: new session.MemoryStore,
-    saveUninitialized: true,
-    resave: 'true',
-    secret: 'secret'
-}))
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.routes.js"],
+};
+
+
+app.use(cors());
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(session({
+//   cookie: { maxAge: 60000 },
+//   store: new session.MemoryStore,
+//   saveUninitialized: true,
+//   resave: 'true',
+//   secret: 'secret'
+// }))
+const specs = swaggerJsdoc(options);
 app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs)
-  );
-app.use(flash());
-app.use('/users', usersRouter);
+  "/api",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, { explorer: true })
+);
+
+// app.use(flash());
+app.use(usersRouter);
+app.use( authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
---
+++ models/user.model.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+        role: {
+            type: Sequelize.INTEGER
+        }
+    });
+
+    return User;
+};
